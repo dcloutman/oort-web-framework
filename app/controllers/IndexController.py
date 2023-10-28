fb40d44d9a5dc9551ce2cdfd46d39707e6f4b598
David Cloutman <david.cloutman@lampfire.com>
2023-10-27 22:16:28 -0700

    Renamed Test1Controller to IndexController.

--- app/controllers/IndexController.py
+++ app/controllers/IndexController.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_classful import FlaskView
 from oort.controllers.BaseController import BaseController
 
-class Test1Controller(BaseController):
+class IndexController(BaseController):
     route_base = "/"
     
     def __init__(self) -> None:
