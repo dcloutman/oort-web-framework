a470e72272b331b0a166682e99dd1d6d38be4b01
David Cloutman <david.cloutman@lampfire.com>
2023-11-02 15:49:42 -0700

    Added a utility class that Flask Login requires.

---
+++ oort/lib/auth/__init__.py
@@ -0,0 +1,26 @@
+class AuthUser():
+    """
+    Flask Login requires a class with this signature to generate the session.
+    """
+    
+    def __init__ (self, user_id):
+        self.is_authorized = True
+        """
+        This property should return True if the user is authenticated, i.e. they have provided valid credentials. (Only authenticated users will fulfill the criteria of login_required.)
+        """
+
+        self.is_active = True
+        """
+        This property should return True if this is an active user - in addition to being authenticated, they also have activated their account, not been suspended, or any condition your application has for rejecting an account. Inactive accounts may not log in (without being forced of course).
+        """
+
+        self.is_anonymous = True
+        """
+        This property should return True if this is an anonymous user. (Actual users should return False instead.)
+        """
+
+        self.user_id = user_id
+
+    def get_id(self):
+        return self.user_id
+    
