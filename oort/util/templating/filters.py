d71984a092b521e15639b1344fbce9e509d47180
David Cloutman <david.cloutman@lampfire.com>
2023-11-04 19:41:38 -0700

    More front-end improvements.

--- oort/util/templating/filters.py
+++ oort/util/templating/filters.py
@@ -16,15 +16,15 @@ def hsdoc( value):
     """HTML Simple Document
     Simple documents, like comments or brief articles. Use when keeping a close reign on your formatting.
 
-    Only allows <strong>, <em>, <br>, <p>, <h2>, and <h3> tags.
+    Allows <strong>, <em>, <br>, <p>, <h2>, and <h3> tags. The <b> and <i> tags are also permitted, though not advised.
     """
-    return bleach.clean(value, ['strong', 'em', 'br', 'p', 'h2', 'h3', 'ul', 'ol', 'li'])
+    return bleach.clean(value, ['strong', 'i', 'b', 'em', 'br', 'p', 'h2', 'h3', 'ul', 'ol', 'li'])
 
 @flask_app.template_filter()
 def hidoc(value):
     """HTML Intermediate Document
     Moderately complex documents, like pages or blog posts. Use when keeping a close reign on your formatting.
 
-    Only allows <strong>, <em>, <br>, <p>, and <h1>-<h6> tags.
+    Allows <strong>, <em>, <br>, <p>, and <h1>-<h6> tags. The <b> and <i> tags are also permitted, though not advised.
     """
-    return bleach.clean(value, ['header', 'strong', 'em', 'br', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li', 'aside', 'section', 'blockquote', 'pre', 'code'])
+    return bleach.clean(value, ['header', 'strong', 'b', 'em', 'i', 'br', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ul', 'ol', 'li', 'aside', 'section', 'blockquote', 'pre', 'code'])
