fe27015a7568cf39f4970cfa0e10f79b3c2c1eb1
David Cloutman <david.cloutman@lampfire.com>
2024-05-14 14:48:41 -0700

    Moved configurations into a common Python file. (config.py)

--- app/config.py
+++ app/config.py
@@ -2,11 +2,32 @@
 ### Flask Standard Configurations ###
 #####################################
 
-SECRET_KEY="X0LFpiJ87U!hB%0o2ku7dyRW?hUB9N7tfO^Ry~Qiqsz/J##R~4)|>7fCMs9nXlwKUf#u31n&TfR{ew^vS)i:s5mOQQi7b4s1MUOmfU-iIH@Nvi>rh5f(I@&yK#H@HA^E"
+SECRET_KEY="Change-me-before-use-X0LFpiJ87U!hB%0o2ku7dyRW?hUB9N7tfO^Ry~Qiqsz/J##R~4)|>7fCMs9nXlwKUf#u31n&TfR{ew^vS)i:s5mOQQi7b4s1MUOmfU-iIH@Nvi>rh5f(I@&yK#H@HA^E"
 """
 A secret key that will be used for securely signing the session cookie and can be used for any other security related needs 
 by extensions or your application. It should be a long random bytes or str.
 """
+
+########################
+### MySQL Connection ###
+########################
+db_name = 'my_app'
+"""
+Change this to something appropriate for your application.
+"""
+
+db_user = 'oort_app'
+"""
+Choose a username to your database for your application that is difficult for adversaries guess. Do not use `root`.
+
+Ideally, you should create a separate user for making schema changes and deny the user listed above the ability to make schema changes to the database.
+"""
+
+db_pw = 'notag00dPW-changeme'
+"""
+Definitely change this password to something highly randomized.
+"""
+
 
 #####################
 ### Oort Specific ###
