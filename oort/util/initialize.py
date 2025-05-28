7f80172bf740f1957ce5e820ddf83dac41e9688d
David Cloutman <david.cloutman@lampfire.com>
2025-05-28 17:53:52 -0500

    Removed support for old config file. Will use .env files moving forward.

--- oort/util/initialize.py
+++ oort/util/initialize.py
@@ -5,14 +5,7 @@ from os import path, environ, pardir
 
 flask_app = Flask(__name__, static_folder="../../static", template_folder="../../templates")
 #CORS(flask_app, resources={r"/*": {"origins": "*"}})
-flask_app.config.from_object('app.config')
 
-# This block will override configurations that have matching environment variables if the config.py
-# file allows it.
-if flask_app.config['OVERRIDE_FROM_ENVIRONMENT_VARIABLES']:
-    for conf_key in flask_app.config:
-        if conf_key in environ:
-            flask_app.config[conf_key] = environ[conf_key]
-
+# Set the app path to the root of the application
 APP_PATH = path.abspath(path.join(path.join(path.dirname(__file__), '..'), '..'))
 flask_app.config['APP_PATH'] = APP_PATH
